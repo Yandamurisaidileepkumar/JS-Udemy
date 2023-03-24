9b6db8386258db2df22250d2fa571e04dff8176f
Yandamuriisaidileepkumar <yandamuridileepkumar16@gmail.com>
2023-03-24 18:46:57 +0530

    okie

---
+++ JS Fundamentals-1/one.js
@@ -0,0 +1,12 @@
+// let $dileep = "surya";
+// console.log($dileep);
+// let dileep = "surya";
+// console.log(_dileep);
+
+// let a = "10";
+// let b = "dileep";
+// console.log(a * b);
+
+let x = 1;
+--x;
+console.log(x);
